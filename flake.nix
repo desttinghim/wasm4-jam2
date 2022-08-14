e00ffec2ed9d7d5b13978f1f06ee8f087a99247d
Louis Pearson <opensource@louispearson.work>
2022-08-13 18:17:17 -0600

    Add wasm-opt, flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    zig.url = "github:arqv/zig-overlay";
+    unstable.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, zig, flake-utils, unstable }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        upkgs = unstable.legacyPackages.${system};
+      in
+      {
+
+        # nix develop
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            zig.packages.${system}.master.latest
+            pkgs.butler
+            pkgs.binaryen
+            upkgs.ldtk
+          ];
+        };
+
+      });
+}
